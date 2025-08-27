7d4b19f01d1aa9c3fc1627e9c2b9d1b7d91375f8
GustavoLVL <gustavomirandaoficiallvl@gmail.com>
2025-08-27 23:25:39 +0000

    Atualização 28/08

---
+++ projeto_integrador_sergio/src/main/java/entidades/Projeto.java
@@ -0,0 +1,24 @@
+package entidades;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
+public class Projeto {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public String nome;
+  public Integer quantidade;
+  public double preco;
+  public LocalDate validade;  
+  
+
+  @ManyToOne
+  public Categoria categoria;
+}
